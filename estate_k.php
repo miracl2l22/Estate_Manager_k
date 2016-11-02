6f6a0d5a89e3d3290abcd996beaf4bc1a6df6da2
miracl2l22 <miracl2l22@gmail.com>
2016-11-02 16:05:12 +0900

    Create estate_k.php

---
+++ estate_k.php
@@ -0,0 +1,15 @@
+/**
+ * Plugin Name: Estate Manger k
+ * Plugin URI: 
+ * Description:
+ * Version: 1.0
+ * Author: miracl2l22
+ * Author URI: http://
+ * Requires at least:
+ * Tested up to: 
+ *
+ * Text Domain: estatek
+ * Domain Path: /languages/
+ */
+ 
+ if ( ! defined( 'ABSPATH' ) ) exit;
